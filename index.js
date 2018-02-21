aba66e2875adee6463fba121c560d0959d0bb678
Toby Ryuk <toby.ryuk@danielwellington.com>
2018-02-21 13:40:35 +0100

    :wrench: Finalize eslint config with room for updates

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 module.exports = {
   extends: 'standard',
+  env: {
+    'es6': true
+  },
+  parserOptions: {
+    'ecmaVersion': 6
+  },
   rules: {
-    'no-var': 2
+    'array-callback-return': [2, { allowImplicit: true }],
+    'arrow-parens': [2, 'as-needed'],
+    'consistent-return': 2,
+    'guard-for-in': 2,
+    'no-else-return': 2,
+    'no-return-await': 2,
+    'no-useless-return': 2,
+    'no-var': 2,
+    'object-shorthand': 2,
+    'prefer-arrow-callback': 2,
+    'prefer-const': 2,
+    'prefer-spread': 2,
+    'prefer-template': 2,
+    'require-await': 2
   }
 }
